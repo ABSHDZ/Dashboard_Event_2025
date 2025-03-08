0f330fc6e1ff8ea7d7f7331512f0220ae0e9422d
Absalón <absalon98hg@gmail.com>
2025-03-08 03:19:57 -0600

    r3

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 from streamlit_gsheets import GSheetsConnection
 #Title
-st.title("Encuentro Juvenil")
+st.title("Dashboard")
 #Refresh
 if st.button("Refrescar"):
     st.cache_resource.clear()
